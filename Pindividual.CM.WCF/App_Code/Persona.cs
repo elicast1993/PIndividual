3c3701a3606429ba70c65382cb7587821b0563db
E <E@E6440>
2019-11-05 20:18:27 -0600

    Avance

---
+++ Pindividual.CM.WCF/App_Code/Persona.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Persona
+/// </summary>
+public class Persona
+{
+    String nombre;
+    String cedula;
+    // DataMember
+    public Persona()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+    [DataMember]
+    public string Nombre
+    {
+        get
+        {
+            return nombre;
+        }
+
+        set
+        {
+            nombre = value;
+        }
+    }
+    [DataMember]
+    public string Cedula
+    {
+        get
+        {
+            return cedula;
+        }
+
+        set
+        {
+            cedula = value;
+        }
+    }
+}
